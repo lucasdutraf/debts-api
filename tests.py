c388336c421f54e76fa66fea20eeffb6cf67d07e
Lucas Dutra <ldutra98@gmail.com>
2021-02-12 12:56:09 -0300

    Update tests according to new file extension

--- tests.py
+++ tests.py
@@ -1,19 +1,14 @@
+import pytest
 import json
 from service import SPService
 from parser import SPParser
+from api import API
 
-# Sinta-se livre para deletar o teste abaixo, caso queira.
-def test_search_ticket():
-    service = SPService(
-        license_plate="license_plate",
-        renavam="renavam",
-        debt_option="ticket"
-    )
-    result = service.debt_search()
-    assert result == True
-
-@pytest.mark.parametrize("license_plate, renavam, debt_option, output_file", [('ABC1234', '11111111111', 'dpvat', './tests/dpvat.out'), ('ABC1234', '11111111111', 'ipva', './tests/ipva.out'), ('ABC1234', '11111111111', 'licensing', './tests/licensing.out'),('ABC1234', '11111111111', 'ticket', './tests/ticket.out'), (('ABC1234', '11111111111', None, './tests/all_debt.out'))])
+@pytest.mark.parametrize("license_plate, renavam, debt_option, output_file", [('ABC1234', '11111111111', 'dpvat', './tests/dpvat.json'), ('ABC1234', '11111111111', 'ipva', './tests/ipva.json'), ('ABC1234', '11111111111', 'licensing', './tests/licensing.json'),('ABC1234', '11111111111', 'ticket', './tests/ticket.json'), (('ABC1234', '11111111111', None, './tests/all_debt.json'))])
 def test_main_flow(license_plate, renavam, debt_option, output_file):
+    with open(output_file, 'r') as f:
+        output_ = json.load(f)
+
     service = SPService(
         license_plate=license_plate,
         renavam=renavam,
@@ -31,10 +26,10 @@ def test_main_flow(license_plate, renavam, debt_option, output_file):
         result = parser.collect_licensing_debts()
     elif debt_option == None:
         result = parser.collect_all_debts()
-    assert result == output_file
+    assert result == output_
 
 
-# @pytest.mark.parametrize("input_file", [('./src/utils/read_file_es.out'), ('./test_utils/read_file_en.out')])
+# @pytest.mark.parametrize("input_file", [('./src/utils/read_file_es.json'), ('./test_utils/read_file_en.json')])
 # def test_read_input_file_not_found(input_file):
 #     with pytest.raises(ArquivoNaoEncontradoException):
 #         parser = Parser()
